544b5600b75521290be43775a17e621f863980c8
Bozhidar Nemski <bojonemski@gmail.com>
2024-11-24 13:13:01 +0200

    test(snapshot-tests): add snapshot tests

--- test/aws-exam.test.ts
+++ test/aws-exam.test.ts
@@ -1,17 +1,9 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsExam from '../lib/aws-exam-stack';
+import * as cdk from 'aws-cdk-lib';
+import { AwsExamStack } from '../lib/aws-exam-stack';
+import 'jest-cdk-snapshot';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws-exam-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new AwsExam.AwsExamStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  const app = new cdk.App();
+  const stack = new AwsExamStack(app, 'MyTestStack');
+  expect(stack).toMatchCdkSnapshot();
 });
