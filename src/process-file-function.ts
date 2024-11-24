40ea30272b21c88d571df6404297219809a4d408
Bozhidar Nemski <bojonemski@gmail.com>
2024-11-24 12:26:10 +0200

    feat(file-upload-function): store successful file uploads in dynamodb and send email

--- src/process-file-function.ts
+++ src/process-file-function.ts
@@ -1,13 +1,17 @@
 import { APIGatewayEvent } from 'aws-lambda';
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
 import { PublishCommand, SNSClient } from '@aws-sdk/client-sns';
 import { fileTypeFromBuffer } from 'file-type';
+import { v4 } from 'uuid';
 
 const snsClient = new SNSClient({});
+const dynamodbClient = new DynamoDBClient({});
 
 export const handler = async (event: APIGatewayEvent) => {
   console.log('process-file-function:event', event);
 
-  const topicArn = process.env.TOPIC_ARN;
+  const tableName = process.env.TABLE_NAME || '';
+  const topicArn = process.env.TOPIC_ARN || '';
 
   try {
     const { body, isBase64Encoded, headers } = event;
@@ -27,11 +31,10 @@ export const handler = async (event: APIGatewayEvent) => {
     const fileName = headers['x-file-name'] || 'unknown';
 
     if (!['pdf', 'jpg', 'png', 'jpeg'].includes(extension)) {
-      await snsClient.send(
-        new PublishCommand({
-          TopicArn: topicArn,
-          Message: `There was a problem with uploading your file: ${fileName}. Please only try to upload files in the following formats: pdf, jpg, and png.`,
-        }),
+      await sendEmailMessage(
+        snsClient,
+        topicArn,
+        `There was a problem with uploading your file: ${fileName}. Please only try to upload files in the following formats: pdf, jpg, and png.`,
       );
 
       return {
@@ -39,6 +42,19 @@ export const handler = async (event: APIGatewayEvent) => {
         body: JSON.stringify({ message: 'Unsupported file type', extension }),
       };
     }
+
+    const uploadDate = new Date(Date.now()).toString();
+    await storeFileInDb(dynamodbClient, tableName, {
+      fileSize: fileSize.toString(),
+      extension: extension.toString(),
+      uploadDate,
+    });
+
+    await sendEmailMessage(
+      snsClient,
+      topicArn,
+      `Your file has been successfully uploaded. File info: file extension - ${extension}, file size - ${fileSize} (in bytes), saved on - ${uploadDate}`,
+    );
 
     return {
       statusCode: 200,
@@ -56,4 +72,45 @@ export const handler = async (event: APIGatewayEvent) => {
       body: JSON.stringify({ message: 'Internal Server Error', error: (error as Error).message }),
     };
   }
+};
+
+const storeFileInDb = async (
+  dynamodbClient: DynamoDBClient,
+  tableName: string,
+  data: { extension: string; fileSize: string; uploadDate: string },
+) => {
+  const { extension, fileSize, uploadDate } = data;
+  const ttl = Math.floor(Date.now() / 1000) * 30 * 60;
+
+  await dynamodbClient.send(
+    new PutItemCommand({
+      TableName: tableName,
+      Item: {
+        id: {
+          S: v4(),
+        },
+        fileSize: {
+          N: fileSize,
+        },
+        fileExtension: {
+          S: extension,
+        },
+        uploadDate: {
+          S: uploadDate,
+        },
+        ttl: {
+          N: ttl.toString(),
+        },
+      },
+    }),
+  );
+};
+
+const sendEmailMessage = async (snsClient: SNSClient, topicArn: string, message: string) => {
+  await snsClient.send(
+    new PublishCommand({
+      TopicArn: topicArn,
+      Message: message,
+    }),
+  );
 };
